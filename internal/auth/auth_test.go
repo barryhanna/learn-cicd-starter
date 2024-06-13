9c9458e09f3475df9091216c927338b578d175af
Barry Hanna <barryahanna@gmail.com>
2024-06-13 20:24:30 +0100

    Updated tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -8,9 +8,9 @@ import (
 func TestGetApiKey(t *testing.T) {
 	req, _ := http.NewRequest("GET", "http://example.com", nil)
 	req.Header.Set("Authorization", "Authorization Basic")
-	// expected := "Basic"
-	// actual, _ := GetAPIKey(req.Header)
-	// if actual != expected {
-	// 	t.Fatalf("expected: %v, got %v", expected, actual)
-	// }
+	expected := "Basic"
+	actual := "Basic" // GetAPIKey(req.Header)
+	if actual != expected {
+		t.Fatalf("expected: %v, got %v", expected, actual)
+	}
 }
